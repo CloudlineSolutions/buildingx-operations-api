7582f7c91295d6ac6752f80c3082350059c7b289
Tom Naylor <tom@tfnaylor.com>
2022-04-20 14:32:11 -0500

    added devices

---
+++ device.go
@@ -0,0 +1,187 @@
+package buildingx
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+type Device struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+	Model        string `json:"model"`
+	Serial       string `json:"serial"`
+	OnlineStatus string `json:"onlineStatus"`
+}
+type SBDevicesResponse struct {
+	Devices []SBDevice `json:"data"`
+}
+type SBDeviceResponse struct {
+	Device SBDevice `json:"data"`
+}
+type SBDevice struct {
+	ID            string                `json:"id"`
+	Attributes    SBDeviceAttributes    `json:"attributes"`
+	RelationShips SBDeviceRelationships `json:"relationships"`
+}
+type SBDeviceRelationships struct {
+	Features SBDeviceFeatures `json:"hasFeatures"`
+}
+type SBDeviceFeatures struct {
+	Data []SBDeviceFeaturesData `json:"data"`
+}
+type SBDeviceFeaturesData struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
+type SBDeviceAttributes struct {
+	ModelName    string `json:"modelName"`
+	SerialNumber string `json:"serialNumber"`
+}
+type SBDevicesIncludedResponse struct {
+	Included []SBDeviceIncluded `json:"included"`
+}
+type SBDeviceIncluded struct {
+	ID            string                        `json:"id"`
+	Type          string                        `json:"type"`
+	Attributes    SBDeviceIncludedAttributes    `json:"attributes"`
+	RelationShips SBDeviceIncludedRelationships `json:"relationships"`
+}
+type SBDeviceIncludedAttributes struct {
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Status      string `json:"status"`
+}
+type SBDeviceIncludedRelationships struct {
+	HasDevice SBDeviceIncludedRelationshipsHasDevice `json:"hasDevice"`
+}
+type SBDeviceIncludedRelationshipsHasDevice struct {
+	Data SBDeviceIncludedRelationshipsData `json:"data"`
+}
+type SBDeviceIncludedRelationshipsData struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
+
+// GetLocations returns an array of devices that are associated with a particular location
+func GetDevicesByLocation(session *Session, locationID string) ([]Device, error) {
+
+	devices := make([]Device, 0)
+
+	// make sure session is initialized
+	if !session.IsInitialized {
+		return devices, errors.New("session is not initialized")
+	}
+
+	// make sure you have the required environment variable
+	endpoint := os.Getenv("BUILDINGX_ENDPOINT")
+	if endpoint == "" {
+		return devices, errors.New("missing buildingx api endpoint")
+	}
+
+	// create the API request
+	path := fmt.Sprintf("devices?include=hasFeatures.DeviceInfo,hasFeatures.Connectivity&filter[hasLocation.data.id]=%s", locationID)
+	req := APIRequest{
+		Partition: session.Partition,
+		JWT:       session.JWT,
+		Path:      path,
+		Operation: GET,
+	}
+
+	// make the API call
+	resp, err := MakeRESTCall(req)
+	if err != nil {
+		return devices, errors.New("error making REST call: " + err.Error())
+	}
+
+	// Unmarshal the native devices response payload
+	sbDevicesResponse := SBDevicesResponse{}
+	if err := json.Unmarshal(resp, &sbDevicesResponse); err != nil {
+		return devices, errors.New("Error parsing API response. String submitted: " + string(resp))
+	}
+
+	// Now unmarshal the device features nodes
+	sbDevicesIncludedResponse := SBDevicesIncludedResponse{}
+	if err := json.Unmarshal(resp, &sbDevicesIncludedResponse); err != nil {
+		return devices, errors.New("Error parsing API response (features section). String submitted: " + string(resp))
+	}
+
+	// now create the Location objects
+	for _, sbDevice := range sbDevicesResponse.Devices {
+
+		device := Device{
+			ID:     sbDevice.ID,
+			Model:  sbDevice.Attributes.ModelName,
+			Serial: sbDevice.Attributes.SerialNumber,
+		}
+		// loop through the device features to populate the rest of the properties
+		for _, sbFeature := range sbDevicesIncludedResponse.Included {
+			if sbFeature.RelationShips.HasDevice.Data.ID == device.ID {
+				if strings.ToLower(sbFeature.Type) == "deviceinfo" {
+					device.Name = sbFeature.Attributes.Name
+					device.Description = sbFeature.Attributes.Description
+				} else if strings.ToLower(sbFeature.Type) == "connectivity" {
+					device.OnlineStatus = sbFeature.Attributes.Status
+				}
+
+			}
+
+		}
+		devices = append(devices, device)
+
+	}
+
+	// all is well. return the devices
+	return devices, nil
+
+}
+
+// func GetSingleLocation(session *Session, id string) (Location, error) {
+
+// 	location := Location{}
+// 	// make sure session is initialized
+// 	if !session.IsInitialized {
+// 		return location, errors.New("session is not initialized")
+// 	}
+
+// 	// make sure you have the required environment variable
+// 	endpoint := os.Getenv("BUILDINGX_ENDPOINT")
+// 	if endpoint == "" {
+// 		return location, errors.New("missing buildingx api endpoint")
+// 	}
+
+// 	// create the API request
+// 	path := fmt.Sprintf("locations/%s", id)
+// 	req := APIRequest{
+// 		Partition: session.Partition,
+// 		JWT:       session.JWT,
+// 		Path:      path,
+// 		Operation: GET,
+// 	}
+
+// 	// make the API call
+// 	resp, err := MakeRESTCall(req)
+// 	if err != nil {
+// 		return location, errors.New("error making REST call: " + err.Error())
+// 	}
+
+// 	// Unmarshal the native location response payload
+// 	sbLocationResponse := SBLocationResponse{}
+// 	if err := json.Unmarshal(resp, &sbLocationResponse); err != nil {
+// 		return location, errors.New("Error parsing API response. String submitted: " + string(resp))
+// 	}
+
+// 	location.ID = sbLocationResponse.Location.ID
+// 	location.Description = sbLocationResponse.Location.Attributes.Description
+// 	location.Name = sbLocationResponse.Location.Attributes.Label
+// 	location.TimeZone = sbLocationResponse.Location.Attributes.TimeZone
+
+// 	// all is well. return the location
+// 	return location, nil
+// 	//TODO: Add coordinates
+// 	//TODO: Add address
+
+// }
---
+++ device_test.go
@@ -0,0 +1,48 @@
+package buildingx
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/aws/aws-xray-sdk-go/xray"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetDevicesByLocation(t *testing.T) {
+	ctx := context.Background()
+	ctx, _ = xray.BeginSegment(ctx, "TestGetDevicesByLocation")
+
+	// first make sure you have a partition ID (from environment variable)
+	partitionID := os.Getenv("BUILDINGX_PARTITION_ID")
+	if partitionID == "" {
+		t.Fatal("unable to find partition ID in environment variable")
+	}
+
+	// initialize the session (uses credentials to authenticate and produce a JWT)
+	session := Session{}
+	err := session.Initialize(partitionID)
+	if err != nil {
+		t.Fatal("test failed while initializing session: ", err.Error())
+	}
+
+	// get the location collection to use in fetching a single valid location
+	// If there are no locations, this will fail
+	locations, err := GetLocations(&session)
+	if err != nil {
+		t.Fatal("error getting locations: ", err.Error())
+	}
+	if len(locations) < 1 {
+		t.Fatal("test failed because there are no valid locations to use")
+	}
+
+	t.Run("get-devices-with-valid-location-id", func(t *testing.T) {
+		devices, err := GetDevicesByLocation(&session, locations[0].ID)
+		if err != nil {
+			t.Fatal("error getting devices: ", err.Error())
+		}
+		// an empty ID on the Location object means that one was not found
+		assert.GreaterOrEqual(t, 1, len(devices))
+	})
+
+}
