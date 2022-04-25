f1cab4abdcb00b25ba851603ad92852fa6adbda6
Tom Naylor <tom@tfnaylor.com>
2022-04-25 14:11:02 -0500

    added location properties

--- location.go
+++ location.go
@@ -11,6 +11,10 @@ type Location struct {
 	ID          string `json:"id"`
 	Name        string `json:"name"`
 	Description string `json:"description"`
+	Street      string `json:"street"`
+	City        string `json:"city"`
+	PostalCode  string `json:"postalCode"`
+	Country     string `json:"country"`
 	TimeZone    string `json:"timeZone"`
 }
 type SBLocationsResponse struct {
@@ -20,13 +24,42 @@ type SBLocationResponse struct {
 	Location SBLocation `json:"data"`
 }
 type SBLocation struct {
-	ID         string               `json:"id"`
-	Attributes SBLocationAttributes `json:"attributes"`
+	ID            string                  `json:"id"`
+	Attributes    SBLocationAttributes    `json:"attributes"`
+	Relationships SBLocationRelationships `json:"relationships"`
 }
 type SBLocationAttributes struct {
 	TimeZone    string `json:"timeZone"`
 	Label       string `json:"label"`
 	Description string `json:"description"`
+}
+type SBLocationRelationships struct {
+	Features SBLocationHasAddress `json:"hasPostalAddress"`
+}
+type SBLocationHasAddress struct {
+	Data SBLocationPostalAddressData `json:"data"`
+}
+type SBLocationPostalAddressData struct {
+	ID   string `json:"id"`
+	Type string `json:"type"`
+}
+type SBLocationIncludedResponse struct {
+	Included []SBLocationIncluded `json:"included"`
+}
+type SBLocationIncluded struct {
+	ID         string                       `json:"id"`
+	Type       string                       `json:"type"`
+	Attributes SBLocationIncludedAttributes `json:"attributes"`
+}
+type SBLocationIncludedAttributes struct {
+	Locality      string `json:"locality"`
+	CountryCode   string `json:"countryCode"`
+	CountryName   string `json:"countryName"`
+	ContinentCode string `json:"continentCode"`
+	ContinentName string `json:"continentName"`
+	Region        string `json:"region"`
+	PostalCode    string `json:"postalCode"`
+	Street        string `json:"street"`
 }
 
 // GetLocations returns an array of all locations associated with the session. It also populates the session object with the locations.
@@ -49,7 +82,7 @@ func GetLocations(session *Session) ([]Location, error) {
 	req := APIRequest{
 		Partition: session.Partition,
 		JWT:       session.JWT,
-		Path:      "locations?filter[type]=Building",
+		Path:      "locations?filter[type]=Building&include=hasPostalAddress",
 		Operation: GET,
 	}
 
@@ -65,6 +98,11 @@ func GetLocations(session *Session) ([]Location, error) {
 		return locations, errors.New("Error parsing API response. String submitted: " + string(resp))
 	}
 
+	sbLocationsIncludedResponse := SBLocationIncludedResponse{}
+	if err := json.Unmarshal(resp, &sbLocationsIncludedResponse); err != nil {
+		return locations, errors.New("Error parsing API response. String submitted: " + string(resp))
+	}
+
 	// now create the Location objects
 	for _, sbLocation := range sbLocationsResponse.Locations {
 
@@ -74,14 +112,22 @@ func GetLocations(session *Session) ([]Location, error) {
 			Description: sbLocation.Attributes.Description,
 			TimeZone:    sbLocation.Attributes.TimeZone,
 		}
+
+		for _, include := range sbLocationsIncludedResponse.Included {
+			if sbLocation.Relationships.Features.Data.ID == include.ID {
+				location.City = include.Attributes.Locality
+				location.Street = include.Attributes.Street
+				location.Country = include.Attributes.CountryCode
+				location.PostalCode = include.Attributes.PostalCode
+			}
+		}
+
 		locations = append(locations, location)
 
 	}
 
 	// all is well. return the locations
 	return locations, nil
-	//TODO: Add coordinates
-	//TODO: Add address
 
 }
 func GetSingleLocation(session *Session, id string) (Location, error) {
@@ -99,7 +145,7 @@ func GetSingleLocation(session *Session, id string) (Location, error) {
 	}
 
 	// create the API request
-	path := fmt.Sprintf("locations/%s", id)
+	path := fmt.Sprintf("locations/%s?include=hasPostalAddress", id)
 	req := APIRequest{
 		Partition: session.Partition,
 		JWT:       session.JWT,
@@ -119,14 +165,26 @@ func GetSingleLocation(session *Session, id string) (Location, error) {
 		return location, errors.New("Error parsing API response. String submitted: " + string(resp))
 	}
 
+	sbLocationsIncludedResponse := SBLocationIncludedResponse{}
+	if err := json.Unmarshal(resp, &sbLocationsIncludedResponse); err != nil {
+		return location, errors.New("Error parsing API response. String submitted: " + string(resp))
+	}
+
 	location.ID = sbLocationResponse.Location.ID
 	location.Description = sbLocationResponse.Location.Attributes.Description
 	location.Name = sbLocationResponse.Location.Attributes.Label
 	location.TimeZone = sbLocationResponse.Location.Attributes.TimeZone
 
+	for _, include := range sbLocationsIncludedResponse.Included {
+		if sbLocationResponse.Location.Relationships.Features.Data.ID == include.ID {
+			location.City = include.Attributes.Locality
+			location.Street = include.Attributes.Street
+			location.Country = include.Attributes.CountryCode
+			location.PostalCode = include.Attributes.PostalCode
+		}
+	}
+
 	// all is well. return the location
 	return location, nil
-	//TODO: Add coordinates
-	//TODO: Add address
 
 }
